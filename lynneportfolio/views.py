6b2d0d384743c4416268116d7bd264f35c402378
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-26 00:07:43 +0300

    added project function

--- lynneportfolio/views.py
+++ lynneportfolio/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
+from .models import Project
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def project(request):
+    try:
+        project = Project.objects.all()
+    except Project.DoesNotExist:
+        raise Http404()
+    return render(request,"project.html", {"project":project})
