0c49c6a7fdcc4d5d707c6951e77d2be7745613b9
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-26 14:15:25 +0300

    register skills

--- lynneportfolio/admin.py
+++ lynneportfolio/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Editor,Project
+from .models import Editor,Project,Skills
 
 # Register your models here.
 
 admin.site.register(Editor)
 admin.site.register(Project)
+admin.site.register(Skills)
 
