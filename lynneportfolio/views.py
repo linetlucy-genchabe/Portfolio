9dcb3b60cc3205a117f9bbd157b5b800fad7b312
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-26 14:16:37 +0300

    make migrations

--- lynneportfolio/views.py
+++ lynneportfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
-from .models import Project
+from .models import Project,Skills
 
 # Create your views here.
 def index(request):
