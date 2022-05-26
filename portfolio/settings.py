95e82cdad7e1a7e0c7c8ff10b562335915401399
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-26 15:03:37 +0300

    deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -34,7 +34,7 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
-if config('MODE')=="dev":
+if config('MODE')=="prod":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
