eb7ac6cebbfed2cb10b689a3112a4e8b341a90ad
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-26 13:11:06 +0300

    skills url

--- lynneportfolio/urls.py
+++ lynneportfolio/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns=[
     
     url(r'^$',views.index,name='index'),
-    url(r'^project/',views.project,name ='project')
+    url(r'^project/',views.project,name ='project'),
+    url(r'^skills/',views.skills,name ='skills'),
+   
     
 ]
